9700fbacc02b6c9b3fddf889a75ee8e0eeeabdab
qywoe <yaniv.grosskopf@mail.huji.ac.il>
2022-04-19 16:42:14 +0300

    Starting EX_3.

---
+++ cmake-build-debug/CMakeFiles/ex3_OS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3_OS.dir/main.cpp.o"
+  "ex3_OS"
+  "ex3_OS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex3_OS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
